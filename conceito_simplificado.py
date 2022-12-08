9ffec54e4a368edb59cdf577f5211c6c12dae2da
RicieriPasqualotto <111090520+Riciler@users.noreply.github.com>
2022-12-08 13:50:15 -0300

    LAB03

---
+++ conceito_simplificado.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+lê um valor para a nota e outro para frequência
+dando o conceito do aluno como saída
+"""
+
+# Entrada de dados
+print('Digite um valor para a nota')
+nota = float(input())
+
+print('Digite um valor para a frequência')
+freq = float(input())
+
+# Controle de entrada
+if nota>100 or nota<0 or freq>100 or freq<0:
+    print('Entrada inválida')
+else:
+    if nota>60 and freq>75:
+        print('Aprovado')
+    elif nota<60 and freq>75:
+        print('Exame')
+    else:
+        print('Reprovado') 
+
+
