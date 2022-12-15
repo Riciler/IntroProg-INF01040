3d5c521ded847ad9165080a83f9acf444e1a1062
RicieriPasqualotto <111090520+Riciler@users.noreply.github.com>
2022-12-15 16:16:44 -0300

    EP04 ok
    EP04 agora funcionando

--- EP04_ovos_pascoa2.py
+++ EP04_ovos_pascoa2.py
@@ -3,56 +3,43 @@
 ovo = input()
 
 try:
-    quant = float(input())
-except:
-    print('Quantidade Inválida')
+    quant = int(input())
+except ValueError:
+    print('Quantidade inválida')
 else:
-    if quant.is_integer() == False or quant <= 0:
-        print('Quantidade Inválida')
+    excs = False
+    if ovo == 'A':
+        prec = 12.0
+        lim = 50
+        if quant > lim:
+            quant = lim
+            excs = True
+        total = quant * prec
+        print(f'Pedido: {quant} ovos do tipo Simples (A)')
+        print('Valor Total: R$','%.2f' % total)
+        if excs==True:
+            print('Limite excedido')
+    elif ovo == 'B':
+        prec = 15.5
+        lim = 30
+        if quant > lim:
+            quant = lim
+            excs = True
+        total = quant * prec
+        print(f'Pedido: {quant} ovos do tipo Recheado (B)')
+        print('Valor Total: R$','%.2f' % total)
+        if excs==True:
+            print('Limite excedido')
+    elif ovo == 'C':
+        prec = 21.3
+        lim = 20
+        if quant > lim:
+            quant = lim
+            excs = True
+        total = quant * prec
+        print(f'Pedido: {quant} ovos do tipo Com surpresa (C)')
+        print('Valor Total: R$','%.2f' % total)
+        if excs==True:
+            print('Limite excedido')
     else:
-        excs = 0
-        if ovo == 'A':
-            prec = 12.0
-            lim = 50
-            if quant > lim:
-                quant = lim
-                excs = 1
-            total = quant * prec
-            print(f'Pedido: {quant:.0f} ovos do tipo Simples (A)')
-            print(f'Valor Total: R$ {total:.2f}')
-            if excs==1:
-                print('Limite excedido')
-        elif ovo == 'B':
-            prec = 15.5
-            lim = 30
-            if quant > lim:
-                quant = lim
-                excs = 1
-            total = quant * prec
-            print(f'Pedido: {quant:.0f} ovos do tipo Recheado (B)')
-            print(f'Valor Total: R$ {total:.2f}')
-            if excs==1:
-                print('Limite excedido')
-        elif ovo == 'C':
-            prec = 21.3
-            lim = 20
-            if quant > lim:
-                quant = lim
-                excs = 1
-            total = quant * prec
-            print(f'Pedido: {quant:.0f} ovos do tipo Com surpresa (C)')
-            print(f'Valor Total: R$ {total:.2f}')
-            if excs==1:
-                print('Limite excedido')
-        else:
-            print('Produto inválido')
-        
-
-
-            
-            
-        
-            
-        
-
-    
+        print('Produto inválido')
